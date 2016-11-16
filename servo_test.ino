2fe2cb9d00d045421d0dd9daf2be98539ef8edf4
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2016-11-15 17:35:10 -0800

    Added comments for clarification

--- servo_test.ino
+++ servo_test.ino
@@ -1,13 +1,17 @@
+//activates the servo library
 #include <Servo.h>
 
+//gives our servo a name
 Servo servo1;
 
 void setup() {
+  //sets the pin to output and attaches servo to it
   pinMode(1, OUTPUT);
   servo1.attach(3);
 }
 
 void loop() {
+  //moves servo 75 deg clockwise
   servo1.write(75);
   delay(50);
   
