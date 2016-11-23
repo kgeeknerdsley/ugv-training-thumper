600b1f8c1eaa930a35612472ea26c879f47be2a5
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2016-11-22 16:18:46 -0800

    Created auto_basic.ino
    File for basic autonomy

---
+++ auto_basic.ino
@@ -0,0 +1,22 @@
+//Autonomy Basic:
+//UGV can drive randomly and not hit walls
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
+
+void motorsStart() {
+  //motor A startup
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  //motor B startup
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
