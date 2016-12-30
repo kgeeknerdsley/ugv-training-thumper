d7d7cc5ae374e7ff46910efd425f6d8fe6b5780e
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2016-12-30 15:08:17 -0800

    File to test pins from the Pi's end

---
+++ pintest.py
@@ -0,0 +1,32 @@
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+
+address = 0x04
+
+def writeNumber(value):
+        bus.write_byte(address, value)
+        return -1
+
+def readNumber():
+        number = bus.read_byte(address)
+        # number = bus.read_byte_data(address, 1)
+        return number
+
+while True:
+        var = input("Enter 1-9: ")
+        if not var:
+                continue
+
+writeNumber(var)
+
+print "RPI: Hi Arduino, I sent you ", var
+
+time.sleep(1)
+
+number = readNumber()
+
+print "Arduino: Hey RPI, I received a digit ", number
+
+print
