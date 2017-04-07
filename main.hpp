3f0ca3af1e829bf6adc4019e1d8d57d379aef763
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2017-04-07 16:33:48 -0700

    Create main.hpp

---
+++ main.hpp
@@ -0,0 +1,26 @@
+
+
+#ifndef main_hpp
+#define main_hpps
+
+#include <iostream>
+#include <stdio.h>
+
+#include "FlyCapture2.h"
+
+
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/features2d.hpp" 
+#include "opencv2/calib3d/calib3d.hpp"
+#include <opencv2/video/background_segm.hpp>
+
+
+
+
+#endif /* main_hpp */
